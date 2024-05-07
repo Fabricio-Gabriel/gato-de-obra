372cb5ecabe6781d49d017d68b53d1f523b7a83e
Fabricio Gabriel De Lima Melquiades <fabricio.melquiades04@gmail.com>
2024-05-07 14:26:59 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,56 @@
+// const body = document.querySelector("body")
+
+// window.addEventListener('scroll', function(){
+//   const navbar = document.querySelector('.navbar');
+//   const navinfo = document.querySelector('.nav-info-container')
+//   const scrollPosition = window.scrollY;
+
+//   if (window.innerWidth > 768) {
+//     if (scrollPosition >= 40) {
+//       navbar.classList.add('fixed');
+//       navinfo.classList.add('scroll-fixed')
+//     } else {
+//       navbar.classList.remove('fixed');
+//       navinfo.classList.remove('scroll-fixed')
+//     }
+//   }
+// });
+
+// aside = document.querySelector('.aside-nav')
+// menuIcon = document.querySelector('.menu-icon')
+// closeIcon = document.querySelector('.aside-x')
+
+// menuIcon.addEventListener('click', () => {
+//     aside.classList.toggle('display-toggle');
+//     body.classList.toggle('overflow-toggle')
+// });
+
+// closeIcon.addEventListener('click', () => {
+//     aside.classList.toggle('display-toggle');
+//     body.classList.toggle('overflow-toggle')
+// });
+
+// window.addEventListener('scroll', function(){
+//   const navbar = document.querySelector('.navbar');
+//   const navinfo = document.querySelector('.nav-info-container')
+//   const scrollPosition = window.scrollY;
+
+//   if (window.innerWidth > 768) {
+//     if (scrollPosition >= 40) {
+//       navbar.classList.add('fixed');
+//       navinfo.classList.add('scroll-fixed')
+//     } else {
+//       navbar.classList.remove('fixed');
+//       navinfo.classList.remove('scroll-fixed')
+//     }
+//   }
+// });
+
+// // var dataAtual = new Date();
+
+// // // Obtém o ano atual
+// // var anoAtual = dataAtual.getFullYear();
+
+// // var elementDate = document.querySelector('.year-date');
+// // elementDate.innerText = anoAtual;
+
---
+++ src/waypoint.js
@@ -0,0 +1,159 @@
+// // function removeActive(){
+// //     navList.forEach(function(e){
+// //         e.classList.remove('active-a');
+// //     });
+// // }
+
+// // var homeNav = new Waypoint({
+// //     element: document.getElementById('alfatv'),
+// //     handler: function(direction) {
+// //       removeActive()
+// //       navList[0].classList.add('active-a')
+// //     },
+// //     offset: '10%'
+// // })
+
+// var mainText = document.querySelector('.main-textos')
+// window.addEventListener('load', function() {
+//     mainText.classList.add('animate')
+// });
+
+// var mainBtn = document.querySelector('.main-buttons')
+// window.addEventListener('load', function() {
+//     mainBtn.classList.add('animate')
+// });
+
+// var sobreText1 = new Waypoint({
+//   element: document.querySelector('.servicos-content-text-1'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var sobreText2 = new Waypoint({
+//   element: document.querySelector('.servicos-content-text-2'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var sobreText3 = new Waypoint({
+//   element: document.querySelector('.servicos-content-text-3'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var frotaCard1 = new Waypoint({
+//   element: document.querySelector('.frota-li-1'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var frotaCard2 = new Waypoint({
+//   element: document.querySelector('.frota-li-2'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 300);
+//   },
+//   offset: '75%'
+// })
+
+// var frotaCard3 = new Waypoint({
+//   element: document.querySelector('.frota-li-3'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 600);
+//   },
+//   offset: '75%'
+// })
+
+// var frotaCard4 = new Waypoint({
+//   element: document.querySelector('.frota-li-4'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 900);
+//   },
+//   offset: '75%'
+// })
+
+// var vantagens = new Waypoint({
+//   element: document.querySelector('.vantagens-container'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var certificacoesCard1 = new Waypoint({
+//   element: document.querySelector('.certificacoes-li-1'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+// var certificacoesCard2 = new Waypoint({
+//   element: document.querySelector('.certificacoes-li-2'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 300);
+//   },
+//   offset: '75%'
+// })
+
+// var certificacoesCard3 = new Waypoint({
+//   element: document.querySelector('.certificacoes-li-3'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 600);
+//   },
+//   offset: '75%'
+// })
+
+// var certificacoesCard4 = new Waypoint({
+//   element: document.querySelector('.certificacoes-li-4'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 900);
+//   },
+//   offset: '75%'
+// })
+
+// var certificacoesCard5 = new Waypoint({
+//   element: document.querySelector('.certificacoes-li-5'),
+//   handler: function(direction) {
+//     let elemento = this.element
+//     setTimeout(function() {
+//       elemento.classList.add('animate')
+//     }, 1200);
+//   },
+//   offset: '75%'
+// })
+
+// var clientes = new Waypoint({
+//   element: document.querySelector('.contain-clientes'),
+//   handler: function(direction) {
+//     this.element.classList.add('animate')
+//   },
+//   offset: '75%'
+// })
+
+
